a25eb0c702304eae5555e57c8fd4701ba72ec45d
bigjoze <joseph_mce@live.co.uk>
2019-02-24 09:10:03 +0000

    Adding files

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+class Header extends Component {
+
+  render() {
+    return (
+      <div className="HeaderBG">
+        <div className="headerContent">
+        <h1>To Do List</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
